440fb86ba6cb595dc644b3c53ee3a108dc873828
tonyle2003 <laptrinhcungtony@gmail.com>
2024-05-05 22:21:10 +0700

    05052024

---
+++ basics/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:basics/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: GradientContainer.purple(),
+    ),
+  ));
+}
